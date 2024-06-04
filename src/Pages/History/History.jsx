eb56b209b6616a88297a489dadd35eb83cf0f651
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-06-04 11:30:16 +0530

    Page Layouts

--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import { useOutletContext } from 'react-router-dom'
+import { Stack, Typography, useMediaQuery } from '@mui/material'
+import { ThemeContext } from '../../Themes/Contexts'
 export default function History() {
+  const screenSize = useMediaQuery('(max-width:768px)')
+  const context=useOutletContext()
+  const themeContext =useContext(ThemeContext)
+  console.log(context,themeContext)
   return (
-    <div>History</div>
+    <Stack sx={{height:'100vh', background:(screenSize && themeContext.mode=='light' ?'linear-gradient(180deg, #F9FAFA 59%, #EDE4FF 100%)':'')}} >
+      {screenSize&&<Navbar handleSidebar={context.handleSidebar}/>}
+      <Typography component={'h1'} variant='p'
+      sx={{textAlign:"center",mt:'10%'}}
+      >
+      Conversation History</Typography>    
+    </Stack>
   )
 }
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import { useOutletContext } from 'react-router-dom'
+import { Stack, useMediaQuery } from '@mui/material'
+import { ThemeContext } from '../../Themes/Contexts'
 
 export default function Home() {
+  const screenSize = useMediaQuery('(max-width:768px)')
+  const context=useOutletContext()
+  const themeContext =useContext(ThemeContext)
+  console.log(context,themeContext)
   return (
-    <div>Home</div>
+    <Stack sx={{height:'100vh', background:(screenSize && themeContext.mode=='light' ?'linear-gradient(180deg, #F9FAFA 59%, #EDE4FF 100%)':'')}} >
+    <Navbar handleSidebar={context.handleSidebar}/>
+
+    <Stack>
+    </Stack>
+
+    </Stack>
   )
 }
